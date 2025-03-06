8feac5a476ea8ff93c1b8496eeb7fd7ab08d66bf
Max <tongweizj@gmail.com>
2025-03-05 23:34:23 -0500

    20250305 update 1. 新的项目结构,合并前后端 2. concurrently, npm run dev 同时起前后台两个项目

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const scheduleJobs = require("./cron-jobs"); // 引入定时任务模块
-
 const app = express();
 
 var corsOptions = {
